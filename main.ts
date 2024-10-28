01d40fe2d0c55aca092315af27e255ff4a19a767
UltimateQuack <flamejantepato77@gmail.com>
2024-10-28 13:55:47 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,8 +3,29 @@ function isTouching (_1: game.LedSprite, _2: game.LedSprite) {
         GAMEOVER_SCREEN.showImage(0)
     }
 }
+function SHOW_SCREEN (SCREEN: string) {
+    if (SCREEN == "GAME_OVER") {
+        GAMEOVER_SCREEN.showImage(0)
+        music.play(DEATH_SFX, music.PlaybackMode.UntilDone)
+    } else {
+        if (SCREEN == "WIN_SCREEN") {
+            WIN_SCREEN.showImage(0)
+            music.play(WIN_SFX, music.PlaybackMode.UntilDone)
+        } else {
+            images.createBigImage(`
+                # # # . . . . # # #
+                . . # . . . . . . #
+                . # # . . . . . # #
+                . . . . . . . . . .
+                . # . . . . . . # .
+                `).scrollImage(1, 200)
+        }
+    }
+}
+let WIN_SFX: SoundExpression = null
+let DEATH_SFX: SoundExpression = null
+let WIN_SCREEN: Image = null
 let GAMEOVER_SCREEN: Image = null
-let PLAYER = game.createSprite(0, 0)
 GAMEOVER_SCREEN = images.createImage(`
     # . . . #
     . # . # .
@@ -12,12 +33,12 @@ GAMEOVER_SCREEN = images.createImage(`
     . # . # .
     # . . . #
     `)
-let WIN_SCREEN = images.createImage(`
+WIN_SCREEN = images.createImage(`
     . . . . #
     . . . # .
     # . # . .
     . # . . .
     . . . . .
     `)
-let DEATH_SFX = music.createSoundExpression(WaveShape.Sawtooth, 1428, 0, 255, 0, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Linear)
-let WIN_SFX = music.createSoundExpression(WaveShape.Triangle, 5000, 0, 255, 0, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Linear)
+DEATH_SFX = music.createSoundExpression(WaveShape.Sawtooth, 1428, 0, 255, 0, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Linear)
+WIN_SFX = music.createSoundExpression(WaveShape.Triangle, 5000, 0, 255, 0, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Linear)
